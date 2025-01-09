ce3b2cf7ab0c7a4921b928e317e585d19be7e81e
ReginaTR <regininhar@hotmail.com>
2025-01-09 11:00:32 -0300

    Renderização das tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { FC } from 'react'
 import './App.css'
 import TaskManager from './components/task_manager'
 
-const App = () => <TaskManager/>
+//Aqui estou tipando o tasks
+export type TaskType = {
+  id: number,
+  title: string,
+  done: boolean
+}
+const TASKS: TaskType[] = [
+  {id: 1, title: 'A', done: true},
+  {id: 2, title: 'B', done: false},
+  {id: 3, title: 'C', done: true},
+
+] 
+
+const App: FC = () => <TaskManager tasks={TASKS}/>
 
 
 export default App
--- src/components/task_manager.tsx
+++ src/components/task_manager.tsx
@@ -1,69 +1,86 @@
-const TaskManager = () => (
-    <>
-    <section className='todoapp'>
-      <header className='header'>
-        <h1>todos</h1>
-        <input className='new-todo' placeholder='What needs to be done?' autoFocus />
-      </header>
+import { FC } from "react"
+import { TaskType } from "../App"
+import classNames from 'classnames'
 
-      <section className='main'>
-        <input id='toggle-all' className='toggle-all' type='checkbox' />
-        <label htmlFor='toggle-all'>Mark all as complete</label>
-        <ul className='todo-list'>
-          <li className='completed'>
-            <div className='view'>
-              <input className='toggle' type='checkbox' />
-              <label>Taste JavaScript</label>
-              <button className='destroy'></button>
-            </div>
-            <input className='edit' value='Taste JavaScript' />
-          </li>
-          <li>
-            <div className='view'>
-              <input className='toggle' type='checkbox' />
-              <label>Buy a unicorn</label>
-              <button className='destroy'></button>
-            </div>
-            <input className='edit' value='Buy a unicorn' />
-          </li>
-        </ul>
-      </section>
 
-      <footer className='footer'>
-        <span className='todo-count'>
-          <strong>0</strong> item left
-        </span>
-        <ul className='filters'>
-          <li>
-            <a className='selected' href='#/'>
-              All
-            </a>
-          </li>
-          <li>
-            <a href='#/active'>Active</a>
-          </li>
-          <li>
-            <a href='#/completed'>Completed</a>
-          </li>
-        </ul>
-        <button className='clear-completed'>Clear completed</button>
-      </footer>
-    </section>
+type PropsType = {
+    tasks: TaskType[]
+}
 
-    <footer className='info'>
-      <p>Double-click to edit a todo</p>
-      <p>
-        Template by <a href='http://sindresorhus.com'>Sindre Sorhus</a>
-      </p>
-      <p>
-        Created by <a href='http://todomvc.com'>you</a>
-      </p>
-      <p>
-        Part of <a href='http://todomvc.com'>TodoMVC</a>
-      </p>
-    </footer>
-  </>
-)
+
+const TaskManager: FC<PropsType> = props => {
+    //console.log(props.tasks) Isso é para teste, nunca será usando em ambiente de desenvolvimento. Foi só para ver se subiu os dados passados.
+
+
+    return (
+        <>
+            <section className='todoapp'>
+            <header className='header'>
+                <h1>todos</h1>
+                <input className='new-todo' placeholder='What needs to be done?' autoFocus />
+            </header>
+
+            <section className='main'>
+                <input id='toggle-all' className='toggle-all' type='checkbox' />
+                <label htmlFor='toggle-all'>Mark all as complete</label>
+                <ul className='todo-list'>
+                {props.tasks.map(task =>
+                    <li className={classNames({completed: task.done})}>
+                        <div className='view'>
+                        <input className='toggle' type='checkbox' checked={task.done} />
+                        <label>{task.title}</label>
+                        <button className='destroy'></button>
+                        </div>
+                        <input className='edit' value='Taste JavaScript' />
+                    </li>
+                )}    
+                <li>
+                    <div className='view'>
+                    <input className='toggle' type='checkbox' />
+                    <label>Buy a unicorn</label>
+                    <button className='destroy'></button>
+                    </div>
+                    <input className='edit' value='Buy a unicorn' />
+                </li>
+                </ul>
+            </section>
+
+            <footer className='footer'>
+                <span className='todo-count'>
+                <strong>0</strong> item left
+                </span>
+                <ul className='filters'>
+                <li>
+                    <a className='selected' href='#/'>
+                    All
+                    </a>
+                </li>
+                <li>
+                    <a href='#/active'>Active</a>
+                </li>
+                <li>
+                    <a href='#/completed'>Completed</a>
+                </li>
+                </ul>
+                <button className='clear-completed'>Clear completed</button>
+            </footer>
+            </section>
+
+            <footer className='info'>
+            <p>Double-click to edit a todo</p>
+            <p>
+                Template by <a href='http://sindresorhus.com'>Sindre Sorhus</a>
+            </p>
+            <p>
+                Created by <a href='http://todomvc.com'>you</a>
+            </p>
+            <p>
+                Part of <a href='http://todomvc.com'>TodoMVC</a>
+            </p>
+            </footer>
+        </>
+    )
+}
 
 
 export default TaskManager
